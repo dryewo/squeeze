66cfda7140199745683bc39c025fce497ed46b25
Dmitrii Balakhonskii <dmitrii.balakhonskii@zalando.de>
2017-12-25 18:07:01 +0100

    Version 0.3.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject squeeze "0.3.0"
+(defproject squeeze "0.3.1"
   :description "Library for config coercion"
   :url "https://github.com/dryewo/squeeze"
   :license {:name "Eclipse Public License"
