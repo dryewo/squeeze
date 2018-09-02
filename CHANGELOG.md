ba3fd9a4165ee8dfc9fac7430cd5134dc47d1eea
Dmitrii Balakhonskii <dnomaid@gmail.com>
2018-09-03 01:46:34 +0200

    add changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,23 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](http://keepachangelog.com)
+and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
+
+
+## [Unreleased]
+### Added
+- _CHANGELOG.md_ created.
+### Changed
+- Something has been changed.
+### Fixed
+- Something has been fixed.
+### Removed
+- Something has been removed.
+
+## 0.3.1 — 2017-12-25
+Released without _CHANGELOG.md_
+
+
+[Unreleased]: https://github.com/dryewo/squeeze/compare/0.3.1...HEAD
--- project.clj
+++ project.clj
@@ -3,18 +3,20 @@
   :url "https://github.com/dryewo/squeeze"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [prismatic/schema "1.1.7"]
+  :dependencies [[prismatic/schema "1.1.9"]
                  [circleci/clj-yaml "0.5.6"]]
-  :plugins [[lein-cloverage "1.0.9"]
-            [lein-shell "0.5.0"]]
+  :plugins [[lein-cloverage "1.0.13"]
+            [lein-shell "0.5.0"]
+            [lein-changelog "0.3.1"]]
   :deploy-repositories [["releases" :clojars]]
   :aliases {"update-readme-version" ["shell" "sed" "-i" "s/\\\\[squeeze \"[0-9.]*\"\\\\]/[squeeze \"${:version}\"]/" "README.md"]}
   :release-tasks [["shell" "git" "diff" "--exit-code"]
                   ["change" "version" "leiningen.release/bump-version"]
                   ["change" "version" "leiningen.release/bump-version" "release"]
+                  ["changelog" "release"]
                   ["update-readme-version"]
                   ["vcs" "commit"]
                   ["vcs" "tag"]
                   ["deploy"]
-                  ["vcs" "push"]])
+                  ["vcs" "push"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0"]]}})
